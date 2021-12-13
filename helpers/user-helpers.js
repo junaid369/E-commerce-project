8247b7f93f40a2cda770da8a63dd7893e894e26c
junaid369 <junaidmuhammed690@gmail.com>
2021-12-13 19:29:34 +0530

    bugs fixed

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,7 +1,6 @@
 
 var db = require('../config/connection')
 var collection = require('../config/collection')
-const bcrypt = require('')
 var bcrypt = require('bcryptjs');
 const objectId = require('mongodb').ObjectID
 const { resolve, reject } = require('promise')
